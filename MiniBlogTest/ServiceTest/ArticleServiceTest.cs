ec776ccddc73a3ee64639f74dec2290542e10234
Kailin Liu <KLiu13@slb.com>
2023-11-07 21:12:30 +0800

    test: make should create article test case success in article service test file

--- MiniBlogTest/ServiceTest/ArticleServiceTest.cs
+++ MiniBlogTest/ServiceTest/ArticleServiceTest.cs
@@ -1,29 +1,47 @@
 using MiniBlog.Model;
+using MiniBlog.Repositories;
 using MiniBlog.Services;
 using MiniBlog.Stores;
+using Moq;
+using System;
 using Xunit;
 
 namespace MiniBlogTest.ServiceTest;
 
 public class ArticleServiceTest
 {
-    // [Fact]
-    // public void Should_create_article_when_invoke_CreateArticle_given_input_article()
-    // {
-    //     // given
-    //     var newArticle = new Article("Jerry", "Let's code", "c#");
-    //     var articleStore = new ArticleStore();
-    //     var articleCountBeforeAddNewOne = articleStore.Articles.Count;
-    //     var userStore = new UserStore();
-    //     var articleService = new ArticleService(articleStore, userStore);
+    private readonly Mock<IArticleRepository> mockArticleRepository;
+    private readonly Mock<IUserRepository> mockUserRepository;
+    private readonly ArticleService articleService;
 
-    //     // when
-    //     var addedArticle = articleService.CreateArticle(newArticle);
+    public ArticleServiceTest()
+    {
+        mockArticleRepository = new Mock<IArticleRepository>();
+        mockUserRepository = new Mock<IUserRepository>();
+        articleService = new ArticleService(mockArticleRepository.Object, mockUserRepository.Object);
+    }
 
-    //     // then
-    //     Assert.Equal(articleCountBeforeAddNewOne + 1, articleStore.Articles.Count);
-    //     Assert.Equal(newArticle.Title, addedArticle.Title);
-    //     Assert.Equal(newArticle.Content, addedArticle.Content);
-    //     Assert.Equal(newArticle.UserName, addedArticle.UserName);
-    // }
+    [Fact]
+    public async void Should_create_article_when_invoke_CreateArticle_given_input_article()
+    {
+        // given
+        //var newArticle = new Article("Jerry", "Let's code", "c#");
+        var newArticle = new Article("xianke", "happy", "csgo");
+        mockArticleRepository.Setup(repository => repository.CreateArticle(It.IsAny<Article>())).Callback<Article>(article => article.Id = Guid.NewGuid().ToString()).ReturnsAsync((Article article) => article);
+        mockUserRepository.Setup(repository => repository.GetUserByName(It.IsAny<string>())).ReturnsAsync((User)null);
+        mockUserRepository.Setup(repository => repository.CreateUser(It.IsAny<User>())).ReturnsAsync((User user) => user);
+
+        //var articleStore = new ArticleStore();
+        //var articleCountBeforeAddNewOne = articleStore.Articles.Count;
+        //var userStore = new UserStore();
+        //var articleService = new ArticleService(articleStore, userStore);
+
+        // when
+        var addedArticle = await articleService.CreateArticle(newArticle);
+
+        // then
+        Assert.Equal(newArticle.Title, addedArticle.Title);
+        Assert.Equal(newArticle.Content, addedArticle.Content);
+        Assert.Equal(newArticle.UserName, addedArticle.UserName);
+    }
 }
