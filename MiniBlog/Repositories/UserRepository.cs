abc1b65b66c0ff9194ff85b179dea0f64185ff05
sjin9 <sjin9@slb.com>
2023-11-07 22:50:45 +0800

    feat: add UserRepository and its interface IUserRepository

--- MiniBlog/Repositories/UserRepository.cs
+++ MiniBlog/Repositories/UserRepository.cs
@@ -1,7 +1,30 @@
-﻿namespace MiniBlog.Repositories
+﻿using MiniBlog.Model;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace MiniBlog.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly IMongoCollection<User> userCollection;
+
+        public UserRepository(IMongoClient mongoClient)
+        {
+            var mongoDatabase = mongoClient.GetDatabase("MiniBlog");
+
+            userCollection = mongoDatabase.GetCollection<User>(User.CollectionName);
+        }
+
+        public async Task<User> GetByName(string name)
+        {
+            return await userCollection.Find(a => a.Name == name).FirstAsync();
+        }
+
+        public async Task<User> Create(User user)
+        {
+            await userCollection.InsertOneAsync(user);
+            return await GetByName(user.Name);
+        }
 
     }
 }
